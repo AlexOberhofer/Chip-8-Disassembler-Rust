78a6bc5760200682d227d22bb19baf53f86fb552
Alex <AlexOberhofer@nc.rr.com>
2018-08-10 12:14:10 -0400

    Comments and README update

--- README.md
+++ README.md
@@ -1,7 +1,61 @@
-# A simple Chip-8 Disassembler
+# Chip-8 Disassembler
 
-A Rust port of my Chip-8 Disassembler
+
+A Chip 8 Disassembler implemented in Rust.
+
+## Prerequisites
+
+
+Building this project requrires both Rust and GNU Make. 
+The project is at this time (August, 2018) built with GNU Make V 4.1 and
+rustc V 1.28.0. However, the project should build without problems on 
+earlier versions of both make and rustc.
+
+## Building
+
+
+Clone this repository
+
+```
+$ git clone https://github.com/AlexOberhofer/Chip-8-Disassembler-Rust.git
+```
+Switch to working directory
+
+```
+$ cd Chip-8-Disassembler-Rust
+```
+
+Build
+
+```
+$ make
+```
+
+The executable will be built in the main project directory. If you wish to 
+change the output location feel free to change the output directory the make 
+file.
+
+## Usage
+
+
+Working directory
+
+```
+$ ./chip-8-disassembler </path/to/rom/>
+```
+
+Other executable location
+
+```
+$ </path/to/executable/> </path/to/rom/>
+```
 
 ## Authors
 
- **Alex Oberhofer**
+
+**Alex Oberhofer**
+
+## License
+
+
+This project is licensed under the GNU GPL 3.0 License - see the [LICENSE.md](LICENSE.md) file for details
--- src/chip-8-disassembler.rs
+++ src/chip-8-disassembler.rs
@@ -1,3 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+// A Chip 8 Binary Dissasembly Program
+// Copyright (C) 2018 Alex Oberhofer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 use std::env;
 use std::io;
 use std::u8;
@@ -5,10 +24,14 @@ use std::vec::Vec;
 use std::io::Read;
 use std::fs::File;
 
+/*
+* Main Function Loop
+*/
 fn main() -> io::Result<()>{
 
     let args: Vec<String> = env::args().collect();
 
+    //read file into buffer
     let mut f = File::open(&args[1]).expect("Unable to read file");
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
@@ -16,8 +39,8 @@ fn main() -> io::Result<()>{
     let mut pc = 0;
     let max = buffer.len();
 
+    //disassemble data
     while pc < max {
-        //disassemble
         disassemble(&buffer, pc);
         pc += 2;
         print!("\n");
@@ -26,21 +49,28 @@ fn main() -> io::Result<()>{
     Ok(())
 }
 
+/*
+* Dissassemble Chip 8 binary opcode
+* buffer - Memory buffer
+* usize - Buffer size
+*/
 fn disassemble(buffer: &[u8], pc: usize) {
 
+    //break apart opcode
     let first = buffer[pc];
     let second = buffer[pc+1];
     let first_nibble = buffer[pc] >> 4;
     let last_nibble = buffer[pc + 1] & 0xf;
     print!("{:04x} {:02x} {:02x} ", (pc + 0x200), first, second);
 
+    //decode opcode
     match first_nibble {
         0x0 =>
             match buffer[pc+1] {
                 0x00 => print!("Empty Address"),
                 0xe0 => print!("{:10}", "CLS"),
                 0xee => print!("{:10}", "RET"),
-                _ => print!("UNKNOWN 0 INSTRUCTION"),
+                _ => print!("Unkown 0 Instruction"),
             },
         0x01 => print!("{:10} ${:01x}{:02x}", "JUMP", buffer[pc] & 0xf, buffer[pc+1]),
         0x02 => print!("{:10} V{:01X}, ${:02x}", "CALL", buffer[pc] & 0xf, buffer[pc+1]),
@@ -51,7 +81,7 @@ fn disassemble(buffer: &[u8], pc: usize) {
         0x07 => print!("{:10} V{:02X}, ${:02x}", "ADDI", buffer[pc] & 0xf, buffer[pc+1]),
         0x08 =>
             match last_nibble {
-                0x0 => print!("{:10} V{:01X} V{:01X}", "LD.", buffer[pc] & 0xf, buffer[pc+1] >> 4),
+                0x0 => print!("{:10} V{:01X} V{:01X}", "MV.", buffer[pc] & 0xf, buffer[pc+1] >> 4),
                 0x1 => print!("{:10} V{:01X} V{:01X}", "OR.", buffer[pc] & 0xf, buffer[pc+1] >> 4),
                 0x2 => print!("{:10} V{:01X} V{:01X}", "AND.", buffer[pc] & 0xf, buffer[pc+1] >> 4),
                 0x3 => print!("{:10} V{:01X} V{:01X}", "XOR.", buffer[pc] & 0xf, buffer[pc+1] >> 4),
@@ -80,13 +110,12 @@ fn disassemble(buffer: &[u8], pc: usize) {
                 0x15 => print!("{:10} DEL, V{:01X}", "MOV", buffer[pc] & 0xf),
                 0x18 => print!("{:10} SND, V{:01X}", "MOV", buffer[pc] & 0xf),
                 0x1e => print!("{:10} I, V{:01X}", "ADD", buffer[pc] & 0xf),
-                0x29 => print!("{:10} I, V{:01X}", "LD", buffer[pc] & 0xf),
-                0x33 => print!("{:10} (I), V{:01X}", "MOV BCD", buffer[pc] & 0xf),
-                0x55 => print!("{:10} (I), V0-V{:01X}", "MEM", buffer[pc] & 0xf),
-                0x65 => print!("{:10} V0-V{:01X}, (I)", "MEM", buffer[pc] & 0xf),
+                0x29 => print!("{:10} I, V{:01X}", "LD SPR", buffer[pc] & 0xf),
+                0x33 => print!("{:10} (I), V{:01X}", "LD BCD", buffer[pc] & 0xf),
+                0x55 => print!("{:10} (I), V0-V{:01X}", "MEMW", buffer[pc] & 0xf),
+                0x65 => print!("{:10} V0-V{:01X}, (I)", "MEMR", buffer[pc] & 0xf),
                 _=> print!("Unknown F Instruction"),
             },
-
 
         _=> print!("UNIMPLEMENTED INSTRUCTION"),
     }
